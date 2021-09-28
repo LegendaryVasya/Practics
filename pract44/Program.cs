5fe1b6209de6dd5fec8cbf7a202ca95d1c7294da
василий алхимов <star.wars.vam.pis@gmail.com>
2021-09-28 22:01:57 +0300

    добавил обобщенный набор и по мелочи

---
+++ pract44/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Globalization;
+namespace pract44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создать Словарь, нечувствительный к регистру и независимый от культуры.
+            ListDictionary list = new ListDictionary(new CaseInsensitiveComparer(CultureInfo.InvariantCulture));
+            // Добавить несколько элементов
+            list["Estados Unidos"] = "United States of America";
+            list["Canada"] = "Canada";
+            list["Espana"] = "Spain";
+            //  Показать результаты
+            Console.WriteLine(list["Estados Unidos"]);
+            Console.WriteLine(list["Canada"]);
+            Console.WriteLine(list["Espana"]);
+        }
+    }
+
+}
+    
+
