f2fdf161c8b8a0564ba8e49b3e1d0bb967eb5abb
василий алхимов <star.wars.vam.pis@gmail.com>
2021-09-28 13:32:48 +0300

    добавил стек

--- pract42/Program.cs
+++ pract42/Program.cs
@@ -20,6 +20,21 @@ namespace pract42
                 Console.WriteLine("From Queue: {0}", obj);
 
             }
+
+            Stack stack = new Stack();
+           
+            stack.Push("First");
+            stack.Push("Second");
+            stack.Push("Third");
+            stack.Push("Fourth");
+
+            while (stack.Count > 0)
+            {
+                object obj = stack.Pop();
+                Console.WriteLine("From Stack: {0}", obj);
+            }
+
+
         }
     }
 }
