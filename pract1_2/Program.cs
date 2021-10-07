991dc10b8f019631f18a157e76eefe749f085767
василий алхимов <star.wars.vam.pis@gmail.com>
2021-10-07 12:35:06 +0300

    Начал практику 1

---
+++ pract1_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pract1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
