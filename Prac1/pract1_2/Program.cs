efed68b141e43c9fe2ac4f4ad4fb77873aa3cc3c
василий алхимов <star.wars.vam.pis@gmail.com>
2021-10-09 21:43:21 +0300

    add 1_2

--- Prac1/pract1_2/Program.cs
+++ Prac1/pract1_2/Program.cs
@@ -2,11 +2,36 @@
 
 namespace pract1_2
 {
-    class Program
+    struct Person
     {
-        static void Main(string[] args)
+        public enum Genders :
+            int
+        { Male, Female };
+        public string firstName;
+        public string lastName;
+        public int age;
+        public Genders gender;
+        public Person(string _firstName, string _lastName, int _age, Genders _gender)
         {
-            Console.WriteLine("Hello World!");
+            firstName = _firstName; lastName = _lastName; age = _age; gender = _gender;
+        }
+
+        public override string ToString()
+        {
+            return firstName + " " + lastName + " (" + gender + "), age " + age;
+        }
+
+
+
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Person p = new Person("Tony", "Allen", 32, Person.Genders.Male);
+                Console.WriteLine(p.ToString());
+
+            }
         }
     }
 }
